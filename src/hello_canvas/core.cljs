947f308d3da0b409aa0d917da74fdf64120186c8
Roger Allen <rallen@gmail.com>
2013-11-01 22:04:45 -0700

    Got a fun animation working.

--- src/hello_canvas/core.cljs
+++ src/hello_canvas/core.cljs
@@ -1,6 +1,41 @@
-(ns hello-canvas.core)
+(ns hello-canvas.core
+  (:require [clojure.browser.dom :as dom]))
 ;; create
 ;;   lein new mies hello-canvas
 ;; run
 ;;   lein cljsbuild auto hello-canvas
 (. js/console (log "Hello Canvas!"))
+
+(def canvas (let [e (dom/get-element "canvas")]
+              (atom {:element e :context (.getContext e "2d")})))
+(def counter (atom 0))
+
+(defn rgb [r g b]
+  (str "rgb(" r "," g "," b ")"))
+
+(defn clear
+  "clear the screen"
+  [ctx]
+  (aset ctx "fillStyle" (rgb 220 220 220))
+  (.fillRect ctx 0 0 800 200))
+
+(defn draw
+  "draw a fun little animating pattern"
+  []
+  (let [ctx (:context @canvas)]
+    (clear ctx)
+    (swap! counter inc)
+    (let [c (/ @counter 10.0)]
+      (.beginPath ctx)
+      (.moveTo ctx 800 0)
+      (.lineTo ctx 0 0)
+      (doall
+       (for [x (range 0 800)]
+         (let [y (+ 100 (* 50 (Math/sin (+ c (/ x 20)))))]
+           (.lineTo ctx x y))))
+      (.closePath ctx)
+      (aset ctx "fillStyle" (rgb 100 100 100))
+      (.fill ctx))))
+
+;; let there be animation
+(js/setInterval draw 50)
